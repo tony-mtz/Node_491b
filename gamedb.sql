bd84e8ea9121f78f6763ccea15962558c12f866e
tony <anthony@amartinez.cc>
2017-03-22 14:56:57 -0700

    working login, leaderboards, submitscore

--- gamedb.sql
+++ gamedb.sql
@@ -1,5 +1,4 @@
-
-use gameserver;
+use gamedb;
 	
 create table if not exists `users` (		
     `userName` varchar(70) not null,
@@ -10,11 +9,18 @@ create table if not exists `users` (
 insert into `users` (`userName`, `userPass`) values ('tony', 'tony');
 insert into `users` (`userName`, `userPass`) values ('jess', 'jess');
 
-create table if not exists scores(
+create table if not exists `scores` (
+	`userName` varchar(70) not null,
+	`score`	int(10) not null,
+    `stamp` timestamp not null default current_timestamp,
+    primary key(username, stamp),
+    foreign key(userName) references `users`(userName)
 	
-
-
-
 ) engine = InnoDB default charset=utf8;
 
+
+
+insert into `scores`(`userName`,`score`) values('tony', 100);
+
 select * from users;
+select * from scores;
--- nodeserver.js
+++ nodeserver.js
@@ -8,12 +8,14 @@ var connection = mysql.createConnection({
    host     : 'localhost',
    user     : 'root',
    password : 'goku562',
-   database : 'gameserver'
+   database : 'gamedb'
  });
 
-
-var jsonParser = bodyParser.json()
-
+//*****************************
+//this shit is awesome
+//parses dat from unity....thank you!!!!!!!!!!!!!!
+var jsonParser = bodyParser.urlencoded({ extended: true })
+//*****************************
 var app = express();
 
 app.use(jsonParser);
@@ -23,163 +25,100 @@ app.get("/", function(req,res){
  res.send("GAME DB");
 });
 
-//used for sending a message
-app.get("/leaderboards",function(req,res) {
-
-  var jsonArray;
-  console.log("in leaderboards..");
-  if(!req.body) return res.sendStatus(400);
-  connection.query('select * from  users', function(err, results) {
-    if (err){
-      console.log("SQL Error : " + err);
-      
-    }else{
-      for (var i  = 0; i < results.length; i++) {
-        //put each message with the users 
-        console.log(i + " user " + results[i]["userName"] );
-        console.log("pass " + results[i]["userPass"]);
-      } 
-      res.send(
-            {'response': 'invaliddat',
-            'message': 'Incorrect username or password',
-            'jwt': ""
-          });
-    }
-  });  
-});
-
-
-
-
-
-
-
-
-app.post('/register', function (req, res) {
-  if (!req.body) return res.sendStatus(400);
-  if(!req.body.username) return res.sendStatus(400);
-  if(!req.body.password) return res.sendStatus(400);
-  if(req.body.username.length < 1) return res.sendStatus(400);
-  if(req.body.password.length < 1) return res.sendStatus(400);
-  
-  let values = {username: req.body.username, password: req.body.password};
-  connection.query('INSERT INTO users SET ?', values, function(err, results) {
-    if (err){
-   
-    }else {
-    //create jwt token
-      console.log(req.body.username +" has just registered...");
-      //token is encoded with hmac SHA256 by default
-     
-    }
-  })
-})
-
 
 //used to verify if the user can sign into app
 app.post("/signIn", function(req, res) {
 
-  if (!req.body) return res.sendStatus(400);
-  if(!req.body.username) return res.sendStatus(400);
-  if(!req.body.password) return res.sendStatus(400);
+  console.log(req.body.userName);
+  console.log(req.body.password);
+
+  //login
  
-  console.log("this is the username :"+ req.body.username )
+  // console.log("this is the username :"+ req.body.username )
   //check here if password and username are the same in database
-  var signInQuery="SELECT EXISTS (SELECT 1 FROM users WHERE username=\"" + 
-      req.body.username + "\" AND password=\"" + req.body.password + "\");";
+  var signInQuery="SELECT EXISTS (SELECT 1 FROM users WHERE userName=\"" + 
+  req.body.userName + "\" AND userPass=\"" + req.body.password + "\");";
 
   connection.query(signInQuery, function(err, results, fields) {
     if (err){
       //error occures, exit function
       console.log('login err: ' + err );
+      console.log(req.body.userName + " : not logged in...")  
       return res.send({'response': 'Error', 'error': 'Login Error'});
     
     }else if(results[0][signInQuery.substring(7, signInQuery.length - 1)]) {
         //create jwt token
         //token is encoded with hmac SHA256 by default
       
-        console.log(req.body.username + " : logged in...")   
-      }
-      else 
-        return res.send(
-            {'response': 'invaliddat',
-            'message': 'Incorrect username or password',
-            'jwt': ""
-          });
-    });
+        console.log(req.body.userName + " : logged in...")   
+        res.send("valid login");
+    }else{
+      console.log(req.body.userName + " : not logged in...")   
+      res.send("not valid");
+    }
+  });
 });
 
 
 //used for sending a message
-app.post("/sendMessage",function(req,res) {
+app.post("/submitScore",function(req,res) {
 
 
  console.log("was able to access!!")
- console.log(req.body.username);
- console.log(req.body.receiverName);
- console.log(req.body.message);
- console.log(req.body.date);
+ console.log(req.body.userName);
+ console.log(req.body.score);
 
   if(!req.body) return res.sendStatus(400);
-  if(!req.body.username) return res.sendStatus(400);
-  if(!req.body.receiverName) return res.sendStatus(400);
-  if(!req.body.message) return res.sendStatus(400);
-  if(!req.body.date) return res.sendStatus(400);
-
-  let values = {username: req.body.username, receiverName: req.body.receiverName,
-                message: req.body.message, timestamp: req.body.date};
-  connection.query('INSERT INTO messages SET ?', values, function(err, results) {
+  if(!req.body.userName){
+    console.log("no username!!!!!!!")
+    return res.sendStatus(400);
+  } 
+ 
+  let values = {userName: req.body.userName, score: req.body.score};
+  connection.query('INSERT INTO scores SET ?', values, function(err, results) {
     if (err){
       console.log("SQL Error : " + err);
-      return res.send({'response ': 'error',
-                       'message': 'message did not go through',
-                       'jwt': ""});
+      return res.send("error submitting scores");
     }else{
-      return res.sendStatus(200);
+      return res.send("success");
     }
+
   });  
 });
 
 //used for the client to retrieve their messages
-app.get("/getMessages", function(req, res) {
+app.get("/highscores", function(req, res) {
 
-  console.log(req.auth.username);
-
-  var getMessageQuery ="SELECT * FROM messages WHERE receiverName=\""+req.auth.username+"\";";
+  
+  var top5 ="SELECT * FROM scores order by score desc limit 5";
   let jsonArray = new Array();
-  connection.query(getMessageQuery, function(err, results, fields) {
-    if (err)
+  connection.query(top5, function(err, results, fields) {
+    if (err){
       //error occures, exit function
       return res.send({'response': 'Error', 'error': err});
-    else {
+    }else {
       //user can have multiple messages waiting for them so create an array to store them all
       console.log("results length : " +results.length);
-      // for (var i  = 0; i < results.length; i++) {
-      //   //put each message with the users 
-      //   console.log("messages " + results[i]["message"] );
-      //   jsonArray[i] = {
-      //     message:results[i]["message"],
-      //     sender:results[i]["username"],
-      //     // timestamp:results[i]["timestamp"]
-      //   }  
-      // }
-        //test with user that has messages
-        jsonArray[0] = {
-          message:results[0]["message"],
-          sender:results[0]["username"],
-          timestamp:results[0]["timestamp"]
+      for (var i  = 0; i < results.length; i++) {
+        //put each message with the users 
+        
+        jsonArray[i] = {
+          userName:results[i]["userName"],
+          score:results[i]["score"],
+          stamp:results[i]["stamp"]
+
+          // timestamp:results[i]["timestamp"]
         }  
-        jsonArray[1] = {
-          message:results[1]["message"],
-          sender:results[1]["username"],
-          timestamp:results[1]["timestamp"]
-        }  
-      console.log(jsonArray);
+       }
+      }  
+      var obj = {"obj": jsonArray};
+      console.log(JSON.stringify(jsonArray));
       //res.send(jsonArray);
-      res.sendStatus(200); //working on the client side to get results
+      return res.send(jsonArray); //working on the client side to get results
   
    
-   }
+     
+  
    });  
  
 });
